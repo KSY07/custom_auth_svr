2862d5eb43537a52bd3f98d5384a419fb8a2b723
Kim Se Young <ksy2008w@naver.com>
2023-01-04 09:53:21 +0900

    first commit

---
+++ src/main/java/com/scarf/authsvr/AuthsvrApplication.java
@@ -0,0 +1,13 @@
+package com.scarf.authsvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthsvrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthsvrApplication.class, args);
+	}
+
+}
